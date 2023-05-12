6c9e73160ea43c34a9c3347583c8a43f2e026b7c
Rayhan Ahmed <72126224+rarayhan@users.noreply.github.com>
2023-05-12 23:15:40 +0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Quiz
+from .models import Question
 
-class QuizForm(forms.ModelForm):
-    class Meta:
-        model = Quiz
-        fields = ['title', ...]
+class TakeQuizForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        quiz = kwargs.pop('quiz')
+        super().__init__(*args, **kwargs)
+        for question in quiz.question_set.all():
+            choices = [(choice.id, choice.choice_text) for choice in question.choice_set.all()]
+            self.fields[f'question_{question.id}'] = forms.ChoiceField(
+                label=question.question_text,
+                widget=forms.RadioSelect,
+                choices=choices,
+                required=True,
+            )
