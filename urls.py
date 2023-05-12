dfd8fb521c1cc7a868169345b06532f6b49a5461
Rayhan Ahmed <72126224+rarayhan@users.noreply.github.com>
2023-05-12 23:18:26 +0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,20 +1,12 @@
-          <th>Score</th>
-        </tr>
-      </thead>
-      <tbody>
-        {% for score in scores %}
-          <tr>
-            <td>{{ score.user.username }}</td>
-            <td>{{ score.score }}</td>
-          </tr>
-        {% empty %}
-          <tr>
-            <td colspan="2">No scores available.</td>
-          </tr>
-        {% endfor %}
-      </tbody>
-    </table>
-  {% else %}
-    <p>No scores available.</p>
-  {% endif %}
-{% endblock %}
+from django.urls import path
+from . import views
+
+app_name = 'quiz'
+
+urlpatterns = [
+    path('create/', views.create_quiz, name='create_quiz'),
+    path('list/', views.quiz_list, name='quiz_list'),
+    path('take/<int:quiz_id>/', views.take_quiz, name='take_quiz'),
+    path('result/<int:quiz_id>/', views.quiz_result, name='quiz_result'),
+    path('leaderboard/<int:quiz_id>/', views.leaderboard, name='leaderboard'),
+]
