53db276594a42e986e919f51e91d746d55cfb86a
Rayhan Ahmed <72126224+rarayhan@users.noreply.github.com>
2023-05-12 23:15:10 +0600

    Update views.py

--- views.py
+++ views.py
@@ -1,23 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import QuizForm
+from .forms import QuizForm, TakeQuizForm
+from .models import Quiz, Score
 
 @login_required
-def create_quiz(request):
+def take_quiz(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    
     if request.method == 'POST':
-        form = QuizForm(request.POST)
+        form = TakeQuizForm(request.POST)
         if form.is_valid():
-            quiz = form.save(commit=False)
-            quiz.creator = request.user
-            quiz.save()
-            return redirect('quiz:quiz_list')
+            score = 0
+            for question in quiz.question_set.all():
+                selected_choice = form.cleaned_data.get(f'question_{question.id}')
+                if selected_choice == question.correct_choice:
+                    score += 1
+            Score.objects.create(user=request.user, quiz=quiz, score=score)
+            return redirect('quiz:quiz_result', quiz_id=quiz_id)
     else:
-        form = QuizForm()
-    return render(request, 'quiz/create_quiz.html', {'form': form})
+        form = TakeQuizForm(quiz=quiz)
+    
+    return render(request, 'quiz/take_quiz.html', {'quiz': quiz, 'form': form})
 
 @login_required
-def quiz_list(request):
-    quizzes = Quiz.objects.all()
-    return render(request, 'quiz/quiz_list.html', {'quizzes': quizzes})
+def quiz_result(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    score = Score.objects.filter(user=request.user, quiz=quiz).first()
+    
+    return render(request, 'quiz/quiz_result.html', {'quiz': quiz, 'score': score})
 
-# Other views for quiz taking, scoring, and leaderboard
+@login_required
+def leaderboard(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    scores = Score.objects.filter(quiz=quiz).order_by('-score')
+    
+    return render(request, 'quiz/leaderboard.html', {'quiz': quiz, 'scores': scores})
